e26316fbf2548336f0e62e5ad031be8462adb070
JoaoGMagri <jgmagri@hotmail.com>
2022-08-31 02:13:30 -0300

    Layout Desktop 1 e 2 implementado linkando com os Outros Layout

---
+++ assets/javascript/script-Layout1.js
@@ -0,0 +1,19 @@
+function entrarNoQuizz() {
+
+    const layoutUm = document.querySelector('.layout1');
+    const layoutDois = document.querySelector('.layout2');
+
+    layoutUm.classList.add('esconde');
+    layoutDois.classList.remove('esconde');
+
+}
+
+function novoQuizz() {
+
+    const layoutUm = document.querySelector('.layout1');
+    const layoutTres = document.querySelector('.layout3');
+
+    layoutUm.classList.add('esconde');
+    layoutTres.classList.remove('esconde');
+
+}
