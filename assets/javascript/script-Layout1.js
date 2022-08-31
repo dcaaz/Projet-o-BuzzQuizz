1d37330ae849b285c3aebcd595e10f81447056a9
JoaoGMagri <jgmagri@hotmail.com>
2022-08-31 17:58:59 -0300

    Começo js no layout 1

--- assets/javascript/script-Layout1.js
+++ assets/javascript/script-Layout1.js
@@ -17,3 +17,52 @@ function novoQuizz() {
     layoutTres.classList.remove('esconde');
 
 }
+
+function pegarQuizz() {
+
+    const quizzes = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    quizzes.then(renderizarQuizzes);
+    quizzes.catch(deuErro);
+
+}
+pegarQuizz();
+
+function renderizarQuizzes(promessa) {
+
+    const quizzes = promessa.data;
+    console.log(quizzes);
+    const quizzDaNet = document.querySelector('.outrosQuizzes .quizz');
+    console.log(quizzDaNet);
+
+    quizzDaNet.innerHTML = '';
+
+    quizzes.forEach(element => {
+         
+        quizzDaNet.innerHTML += `
+        
+        <div class="umQuizz ${element.id}" onclick="entrarNoQuizz()">
+            <div class="nomeDoQuizz">${element.title}</div>
+        </div>
+
+        `;
+
+        const umQuizzDaNet = document.querySelector(`.outrosQuizzes .quizz .${element.id}`);
+        console.log(umQuizzDaNet);
+    
+        quizzes.forEach(element => {
+             
+            umQuizzDaNet.style.backgroundImage = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 65.1%, #000000 100%), url('${element.image}')` ;
+    
+        });
+
+    });
+
+
+
+}
+
+function deuErro() {
+
+    conslode.log("Erro");
+
+}
