982066e2bef4fe74bb961735f9f23021f52791d5
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-08-31 21:47:03 -0300

    script layout2, busca quizzes na api, definicao objetos(quizz)

--- assets/javascript/script-Layout2.js
+++ assets/javascript/script-Layout2.js
@@ -7,3 +7,72 @@ function retornarAoLayoutDoisPAraUm() {
     layoutDois.classList.add('esconde');
 
 }
+
+//quizzes da api
+let quizzes;
+
+//estrutura quizz
+let quizz = {
+    title:'',
+    image:'',
+    // perguntas
+    questions:[
+        {
+            title:'',
+            color:'',
+            answers:[
+                {
+                    text: '',
+				    image: '',
+				    isCorrectAnswer: true
+                },
+                {
+                    text: '',
+				    image: '',
+				    isCorrectAnswer: true
+                },
+                {
+                    text: '',
+				    image: '',
+				    isCorrectAnswer: true
+                },
+                {
+                    text: '',
+				    image: '',
+				    isCorrectAnswer: true
+                }
+            ]
+        }
+    ],
+    //nível (nota) de acertos
+    levels:[
+        {
+            title: '',
+            image: '',
+            text: '',
+            minValue: 0
+        },
+        {
+            title: '',
+            image: '',
+            text: '',
+            minValue: 50
+        }
+    ]
+};
+
+//buscando quizzes da api
+const promise_quizzes = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+
+promise_quizzes.then(sucessoQuizz);
+promise_quizzes.catch(trataErroQuizz);
+
+
+function sucessoQuizz(resposta){
+    quizzes = resposta.data;
+
+}
+
+function trataErroQuizz(erro){
+    console.log(erro.response.status);
+}
