9dd3026aa896d704b46c4ff2142d2adae7ec8f97
JoaoGMagri <jgmagri@hotmail.com>
2022-08-31 21:18:59 -0300

    Chamada do axios

--- assets/javascript/script-Layout1.js
+++ assets/javascript/script-Layout1.js
@@ -18,7 +18,7 @@ function novoQuizz() {
 
 }
 
-/* function pegarQuizz() {
+function pegarQuizz() {
 
     const quizzes = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     quizzes.then(renderizarQuizzes);
@@ -30,10 +30,11 @@ pegarQuizz();
 function renderizarQuizzes(promessa) {
 
     const quizzes = promessa.data;
-    console.log(quizzes);
+    
+     console.log(quizzes);
     const quizzDaNet = document.querySelector('.outrosQuizzes .quizz');
     console.log(quizzDaNet);
-
+    /*
     quizzDaNet.innerHTML = '';
 
     quizzes.forEach(element => {
@@ -46,11 +47,11 @@ function renderizarQuizzes(promessa) {
 
         `;
 
+        const umQuizzDaNet = document.querySelector(`.outrosQuizzes .quizz .umQuizz`);
 
+        umQuizzDaNet.style.backgroundImage = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 65.1%, #000000 100%), url('${element.image}')` ;
 
-    });
-
-
+    }); */
 
 }
 
@@ -58,7 +59,7 @@ function deuErro() {
 
     conslode.log("Erro");
 
-} */
+}
 /* const umQuizzDaNet = document.querySelector(`.outrosQuizzes .quizz .${element.id}`);
 console.log(umQuizzDaNet);
 
